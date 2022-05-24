3640ed315772e3d698d50bae3ea00829d6bc9b6e
TW - IT - Tony Ng <hokan@shoalter.com>
2022-05-24 09:37:50 +0800

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <CalculatorComponent />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/calculator">Calculator</router-link>
+  </nav>
+  <router-view />
 </template>
-
-<script>
-import CalculatorComponent from "./components/CalculatorComponent.vue";
-
-export default {
-  name: "App",
-  components: {
-    CalculatorComponent,
-  },
-  data() {
-    return {
-      test: "",
-    };
-  },
-};
-</script>
 
 <style>
 #app {
@@ -25,6 +14,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/CalculatorView.vue
@@ -0,0 +1,15 @@
+<template>
+  <CalculatorComponent />
+</template>
+
+<script>
+// @ is an alias to /src
+import CalculatorComponent from "@/components/CalculatorComponent.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    CalculatorComponent,
+  },
+};
+</script>
